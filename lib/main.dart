c2ec6dc6933c057b5fe9a5a0daf6f96bd355ef33
Henrique da Gama Saczkowski <henrique1443@hotmail.com>
2022-10-23 17:33:21 -0300

    Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'minhas_vacinas.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MinhasVacinas());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
+
+
 
